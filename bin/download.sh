29a715e2496796a4827ff0ed4467988450857270
Felix Tjandrawibawa <cemeng@gmail.com>
2016-04-19 12:03:49 +1000

    Update config to conf.d - after nginx upgrade to 1.9.14

--- bin/download.sh
+++ bin/download.sh
@@ -1,12 +1,6 @@
 ssh felixt@ninkynonk 'sudo cat /etc/nginx/nginx.conf' > nginx/nginx.conf
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-available/brooklynnets-indo.com ' > nginx/sites-available/brooklynnets-indo.com
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-available/default' > nginx/sites-available/default
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-available/felixt.org' > nginx/sites-available/felixt.org
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-available/passionatedevelopment.com' > nginx/sites-available/passionatedevelopment.com
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-available/runningitis.com' > nginx/sites-available/runningitis.com
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-available/tjandrawibawa.org' > nginx/sites-available/tjandrawibawa.org
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-enabled/brooklynnets-indo.com ' > nginx/sites-enabled/brooklynnets-indo.com
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-enabled/felixt.org' > nginx/sites-enabled/felixt.org
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-enabled/passionatedevelopment.com' > nginx/sites-enabled/passionatedevelopment.com
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-enabled/runningitis.com' > nginx/sites-enabled/runningitis.com
-ssh felixt@ninkynonk 'sudo cat /etc/nginx/sites-enabled/tjandrawibawa.org' > nginx/sites-enabled/tjandrawibawa.org
+ssh felixt@ninkynonk 'sudo cat /etc/nginx/conf.d/brooklynnets-indo.com.conf' > nginx/conf.d/brooklynnets-indo.com.conf
+ssh felixt@ninkynonk 'sudo cat /etc/nginx/conf.d/felixt.org.conf' > nginx/conf.d/felixt.org.conf
+ssh felixt@ninkynonk 'sudo cat /etc/nginx/conf.d/passionatedevelopment.com.conf' > nginx/conf.d/passionatedevelopment.com.conf
+ssh felixt@ninkynonk 'sudo cat /etc/nginx/conf.d/runningitis.com.conf' > nginx/conf.d/runningitis.com.conf
+ssh felixt@ninkynonk 'sudo cat /etc/nginx/conf.d/tjandrawibawa.org.conf' > nginx/conf.d/tjandrawibawa.org.conf
